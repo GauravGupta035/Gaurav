21e3d0cdc92b4e1fa073d89f480b1d8a2170352e
Gaurav Gupta <guptagaurav035@gmail.com>
2021-02-04 18:56:40 +0530

    Activity 2

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,18 @@
 //Write a program to find the sum of n different numbers using 4 functions
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, total;
+
+    printf("Enter first Number: ");
+    scanf("%d", &num1);
+    printf("Enter second Number: ");
+    scanf("%d", &num2);
+
+    total = num1 + num2;
+
+    printf("Sum of %d and %d is %d", num1, num2, total);
+
+    return 0;
+}
