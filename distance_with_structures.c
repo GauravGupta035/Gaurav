7c2a7fc4fb19a99cadcf106ee99a1feade70e79d
Gaurav Gupta <guptagaurav035@gmail.com>
2021-02-05 15:08:44 +0530

    Activity 7

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,1 +1,46 @@
 //WAP to find the distance between two points using structures and 4 functions.
+#include <stdio.h>
+#include <math.h>
+
+struct coordinates
+{
+    float x, y;
+};
+typedef struct coordinates Point;
+
+Point input()
+{
+    Point p;
+    printf("Enter x coordinate: ");
+    scanf("%f", &p.x);
+    printf("Enter y coordinate: ");
+    scanf("%f", &p.y);
+
+    return p;
+}
+
+void output(float dist)
+{
+    printf("Distance between the 2 points is %f", dist);
+}
+
+float distanceCalc(Point A, Point B)
+{
+    float distance;
+    distance = sqrt((pow(A.x - B.x, 2)) + (pow(A.y - B.y, 2)));
+
+    return distance;
+}
+
+int main()
+{
+    Point A, B;
+    float dist;
+
+    A = input();
+    B = input();
+    dist = distanceCalc(A, B);
+    output(dist);
+
+    return 0;
+}
