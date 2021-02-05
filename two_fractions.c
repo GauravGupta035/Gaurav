899b383ff6bb46e850a9e4bb380077bccae1ec13
Gaurav Gupta <guptagaurav035@gmail.com>
2021-02-05 16:17:54 +0530

    Activity 8

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,45 @@
 //WAP to find the sum of two fractions.
+#include <stdio.h>
+#include <math.h>
+
+struct fraction
+{
+    int num, deno;
+};
+typedef struct fraction Fraction;
+
+Fraction input()
+{
+    Fraction f;
+    printf("Enter numerator: ");
+    scanf("%d", &f.num);
+    printf("Enter denominator: ");
+    scanf("%d", &f.deno);
+
+    return f;
+}
+
+void output(Fraction sum)
+{
+    printf("The sum is %d/%d", sum.num, sum.deno);
+}
+
+Fraction addition(Fraction A, Fraction B)
+{
+    Fraction sum;
+    sum.deno = A.deno * B.deno;
+    sum.num = (A.num * B.deno) + (B.num * A.deno);
+
+    return sum;
+}
+
+int main()
+{
+    Fraction A, B, s;
+    A = input();
+    B = input();
+    s = addition(A, B);
+    output(s);
+
+    return 0;
+}
